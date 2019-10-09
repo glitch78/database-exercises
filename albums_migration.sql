f6a819ab5886d3c10be4d0ffbc2da64ace4aebe4
Lancelot Ellis <lancelotellis0126@gmail.com>
2019-10-09 16:49:32 -0500

    add updates to the exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -5,7 +5,8 @@ CREATE TABLE albums (
     id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(50) NOT NULL,
     name  VARCHAR(50) NOT NULL,
-    release_date INT UNSIGNED,
-    sales FLOAT NOT NULL,
+    release_date INT(4) UNSIGNED,
+    sales FLOAT(5, 2) UNSIGNED,
+    genre VARCHAR(255),
     PRIMARY KEY (id)
 );
