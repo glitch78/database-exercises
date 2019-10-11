aac86bee9317a4b0f210e32ab2f5a8bdfac02bf2
Lancelot Ellis <lancelotellis0126@gmail.com>
2019-10-11 12:01:40 -0500

    add order by exercise

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,16 +1,43 @@
 USE employees;
 
 #  Find all employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN).
+#Modify your first query to order by first name. The first result should be Irena Reutenauer and the last result should be Vidya Simmen.
 
 select *
 from employees
-where first_name in ('Irena', 'Vidya', 'Maya');
+where first_name in ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name;
+
+#Update the query to order by first name and then last name.
+# The first result should now be Irena Acton and the last should be Vidya Zweizig.
+
+select *
+from employees
+where first_name in ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name , last_name;
+
+#Change the order by clause so that you order by last name before first name.
+# Your first result should still be Irena Acton but now the last result should be Maya Zyda.
+
+select *
+from employees
+where first_name in ('Irena', 'Vidya', 'Maya')
+ORDER BY last_name ,first_name ;
 
 # Find all employees whose last name starts with 'E' — 7,330 rows.
+
+
 
 select *
 from employees
 where last_name like 'e%';
+
+#Update your queries for employees with 'E' in their last name to sort the results by their employee number.
+# Your results should not change!
+
+select * from employees where last_name like 'e%' order by emp_no ;
+
+#Now reverse the sort order for both queries.
 
 # Find all employees hired in the 90s — 135,214 rows.
 
@@ -35,8 +62,8 @@ where last_name like '%q%';
 
 select * from employees
 where first_name = 'Irena'
-    OR  first_name = 'Vidya'
-    OR first_name = 'Maya';
+   OR  first_name = 'Vidya'
+   OR first_name = 'Maya';
 #Add a condition to the previous query to find everybody with those names who is also male — 441 rows.
 
 select * from employees
